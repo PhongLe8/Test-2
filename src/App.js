3a78993bb21d7e8217ba65a5c007be3cb4382db9
PhongLe8 <pthetroller@gmail.com>
2023-12-02 21:29:13 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
+
+
+
 function App() {
+
+  const todoText = useRef();
+
+  useEffect(() => { 
+    const existingTodos = localStorage.getItem('todos');
+    if(existingTodos) {
+      setTodos(JSON.parse(existingTodos));
+    }
+    else
+    setTodos([]);
+  }, []);
+
+  function addTodo(event) {
+    event.preventDefault();
+    const next = [...todos, todoText.current.value];
+    setTodos(next);
+    localStorage.setItem('todos',JSON.stringify(next));
+
+    const [todos, setTodos] = useState([])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {todos.map(todo => (<li key={todo}>{todo}</li>))}
+      </ul>
+
+      <form onSubmit={addTodo}>
+        <input ref={todoText}/>
+        <input type="text" placeholder="Write Task here"/>
+        <input type="submit" value="Add Todo"/> 
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+
+
+
